bc719873a8dc1c3152003e6c5a6e9017fd32a264
DXM <846373685@qq.com>
2019-06-28 11:27:08 +0800

    push

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+import Icon from './icon.jpg';
+import _ from 'lodash'
+import printMe from './print'
+
+function component(){
+    var element = document.createElement('div')
+    var btn = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+
+    return element
+}
+
+document.body.appendChild(component())
